404d4f8ac8687621a350a58e3c51401d2af58f4c
Vicent Marti <tanoku@gmail.com>
2014-09-19 15:03:06 +0200

    Maximum kekker

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source "https://rubygems.org"
 
 gem "rake"
 gem "minitest", "4.7.0"
-gem "shoulda"
-gem "mocha"
+gem "shoulda", "2.11.3"
+gem "mocha", "0.9.12"
 
 gemspec
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 require 'test/unit'
 require 'shoulda'
-require 'mocha/test_unit'
+require 'mocha'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
